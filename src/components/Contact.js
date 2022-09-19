a0b4f2d8cad9c10cabacd19b8377f48999e5682c
Julian Lopez <julianlopez6850@gmail.com>
2022-09-19 05:06:27 -0400

    Working Contact Form using EmailJS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,45 @@
 import React, { useRef } from "react";
-// import emailjs from "@emailjs/browser";
+import emailjs from "@emailjs/browser";
 import "../styles/Contact.css";
+import ContactBackground from "../assets/ContactBackground.png";
 
 function Contact() {
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+
+
+  
+      emailjs.sendForm('service_57trngd', 'template_ng3zzjf', form.current, 'hxRFoH9Rffcch7ieB')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
     return (
         <div className="contact" id="contact">
             <div className = "header">
                 Contact <strong>Me</strong>
             </div>
             <div className = "content">
-                =-=-=-=-=-=-=-=-=-= Under Construction =-=-=-=-=-=-=-=-=-=
+                <form ref={form} onSubmit={sendEmail}>
+                    <div className="contact-form">
+                        <div className = "left">
+                            <input type="text" placeholder="Name *" name="user_name" required />
+                            <input type="email" placeholder="Email *" name="user_email" required />
+                            <input type="text" placeholder="Phone" name="user_phone" />
+                        </div>
+                        <div className = "right">
+                            <textarea placeholder="Message *" name="message" required />
+                        </div>
+                    </div>
+                    <div className="send-button">
+                        <input type="submit" value="Send" />
+                    </div>
+                </form>
             </div>
         </div>
     )
