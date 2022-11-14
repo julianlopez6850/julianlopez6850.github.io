1444cef80f9ded1f4ee7d8c05d53efdf473639e5
Julian Lopez <julianlopez6850@gmail.com>
2022-11-13 20:13:40 -0500

    Small Update
    Updated Resume, Wordle Clone page.
    
    Fixed Homepage About section height.

--- src/pages/WordleClone.js
+++ src/pages/WordleClone.js
@@ -27,14 +27,16 @@ function ThisWebsite() {
 									Wordle Clone is a reproduction of the popular word game website Wordle. The original game was created by Josh Wardle, 
 									a Welsh software engineer. It became heavily popularized in late 2021, and was eventually sold to the New York Times.
 									{"\n\n"}
-									I am creating Wordle Clone to improve my front-end web development skills,
-									and also to gain experience with a backend tecknology. It is being built using
-									{" "}<strong>JavaScript</strong>, <strong>ReactJS</strong>, <strong>CSS</strong>, and <strong>HTML</strong>.
-									I plan on using <strong>Firebase</strong> for the game's backend and database to store a player's login, statistics, and settings.
+									I am creating Wordle Clone to improve my front-end web development skills. 
+									It is being built using <strong>JavaScript</strong>, <strong>ReactJS</strong>, <strong>CSS</strong>, and <strong>HTML</strong>.
 									{"\n\n"}
-									My recreation of the game is currently still <b>under development</b>. In its current form,
-									the game is infinitely playable, lacking the unique daily word, which is the defining feature of the original game.
+									My recreation of the game is currently still <b>under development</b>. The game is infinitely playable
+									in it current form, but it lacks the unique daily word, the defining feature of the original game.
 									It is also missing settings, statistics, and other key features, which I plan on implementing in the near future.
+									{"\n\n"}
+									I originally planned to use a backend technology to store game data; however, 
+									I believe that JavaScript's <b>localStorage</b> object is better suited for this game 
+									to store a player's login, settings, statistics, etc. locally on the user's browser.
 									{"\n\n"}
 								</text1>
 							}
